546161e7692095f63b5fb83f5f3310c73abedc99
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-30 23:24:43 +0530

    done

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -3,18 +3,34 @@
     <v-card-title>Login Form</v-card-title>
     <v-card-text>
       <v-form>
-        <v-text-field label="Username"></v-text-field>
-        <v-text-field label="Password"></v-text-field>
+        <v-text-field label="Username" v-model="username"></v-text-field>
+        <v-text-field label="Password" v-model="password"></v-text-field>
       </v-form>
     </v-card-text>
     <v-card-actions>
-      <v-btn color="primary">Login</v-btn>
+      <v-btn color="primary" @click="onSubmit">Login</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    onSubmit: function () {
+      const formData = {
+        username: this.username,
+        password: this.password,
+      };
+      console.log("Form submitted", formData);
+    },
+  },
+};
 </script>
 
 <style>
