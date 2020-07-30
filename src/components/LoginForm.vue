6fa6311b476c6237f41e9baccf0f202d8bd923a7
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-30 23:20:56 +0530

    done

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    <v-card-title>Login Form</v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field label="Username"></v-text-field>
+        <v-text-field label="Password"></v-text-field>
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="primary">Login</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
